ab8b5ddcdeac96ed1217c7468e35a36c63132699
Marcus Augustus <caracallarex@gmail.com>
2014-10-24 20:37:25 -0700

    Add RSpec directories

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleApp::Application.config.secret_key_base = 'f8d26b1d925cfce4c32cad40f267ec1ab1651820669598fc95facb512d9da525de7cb2e0eee37964a9d8d8a8db8ed35667ae46371f0bcd267cabc7d72b2320d0'
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+
+	if File.exist?(token_file)
+		#Use existing token
+		File.read(token_file).chomp
+	else
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
