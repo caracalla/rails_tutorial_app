b284852059952b06733c5eecd5c23b18a7f9f060
Marcus Augustus <caracallarex@gmail.com>
2014-11-09 23:00:45 -0800

    Add followers/following

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
+
+	#this works because the microposts table has 'user_id', rails infers
+	#association by <class>_id (called a foreign key)
 	has_many :microposts, dependent: :destroy
+
+	#for relationships, must specify connection to User since the foreign key is
+	#'follower_id' and not 'user_id'
+	has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+	has_many :followed_users, through: :relationships, source: :followed
+
+	#by passing in "followed_id" instead of "follower_id", we can reverse the
+	#connection and create reverse_relationships
+	has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+	has_many :followers, through: :reverse_relationships, source: :follower
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
@@ -22,6 +35,18 @@ class User < ActiveRecord::Base
 
 	def feed
 		Micropost.where("user_id = ?", id)
+	end
+
+	def following?(other_user)
+		relationships.find_by(followed_id: other_user.id)
+	end
+
+	def follow!(other_user)
+		relationships.create!(followed_id: other_user.id)
+	end
+
+	def unfollow!(other_user)
+		relationships.find_by(followed_id: other_user.id).destroy
 	end
 
 	private
